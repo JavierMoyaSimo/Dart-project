7a54285e3d53eb70608a1627c5928744676f8978
Javi <javiermoyasimo@gmail.com>
2023-03-31 21:07:39 +0200

    My first comments on dart

--- first-dart/bin/dev.dart
+++ first-dart/bin/dev.dart
@@ -1,5 +1,24 @@
-
 void main(List<String> arguments) {
   print("dev mode");
   print(arguments);
+  print(sum(4, 5));
+  
 }
+
+///return the sum of a with b
+///
+///[a] must an integer
+///
+///[b] must an integer
+///```
+///sum(2,3) = 5
+///```
+///```JSON
+///{"comentario": "json", "coment":"json"}
+///```
+int sum(int a, int b) {
+  return a + b;
+}
+
+
+
--- first-dart/bin/prod.dart
+++ first-dart/bin/prod.dart
@@ -2,4 +2,12 @@
 void main(List<String> arguments) {
   print("prod mode");
   print(arguments);
+  final cat = Pet();
+  cat.age;
+}
+
+///this class represents the logic for all pets
+class Pet {
+  ///this is the age of pet
+  int age = 1;
 }
