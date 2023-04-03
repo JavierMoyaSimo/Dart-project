26131d6ed7805d2beafc3921302a3fe62884a864
Javi <javiermoyasimo@gmail.com>
2023-04-03 20:05:27 +0200

    Datatypes added. i

---
+++ first-dart/bin/datatypes.dart
@@ -0,0 +1,40 @@
+// TIPOS DE DATOS
+
+
+// PRIMITIVES
+/*
+int         números enteros
+double      números flotantes
+bool        true    -    false
+String      cadena de caracteres
+ */
+
+
+
+// DYNAMIC TYPES
+/*
+dynamic
+(Puede cambiar en tiempo de ejecución)
+*/
+
+
+//INSTANCE OF A CLASS
+
+
+//¿Como definir una variable?
+
+/*No está permitido:  XXXXXXXXXXX          -int 5age;   NO EMPEZAR CON NUMERO
+                     XXXXXXXXXXX          -int @age;   NO USAR CARACTER ESPECIAL
+                                    
+ Si está permitido: VVVVVVVVVVVVVV        - int _5age;     SI EMPEZAMOS CON _                                    -
+                                          -int camelCase;   USAR CAMELCASE
+  */
+
+  int ageOfJavi = 28;
+  double price = 0.99;
+  bool enabled = true;
+  String userName = "Javi";
+
+  //DEFINIR MULTIPLES VARIABLES ( del mismo tipo ) EN UNA SOLA LINEA
+
+  String userName1 = "Javi", userLastName1 ="Moya";
