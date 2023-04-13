e6047e16fe2e4f415b61c86799be70673394bdbf
Javi <javiermoyasimo@gmail.com>
2023-04-13 13:04:24 +0200

    feat: Combine cases of switch

--- first-dart/bin/conditionals.dart
+++ first-dart/bin/conditionals.dart
@@ -27,36 +27,55 @@ void main() {
   }
 
   //SWITCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //En una instancia de Datetime, podemos usar .weekday para RECUPERAR el DIA DE LA SEMANA. Nos DEVUELVE 1 (para Lunes), 2 (para Marts), 3 Miercoles...
+  //En una instancia de Datetime, podemos usar .weekday para RECUPERAR el DIA DE LA SEMANA. Nos DEVUELVE 1 (para Lunes), 2 (para Marts), 3 Miercoles...
   final dayOfWeek = DateTime.now().weekday;
 
   switch (dayOfWeek) {
     case 1:
       print("Today is Monday");
       break;
-      case 2:
+    case 2:
       print("Today is Tuesday");
       break;
-      case 3:
+    case 3:
       print("Today is Wednesday");
       break;
-      case 4:
+    case 4:
       print("Today is Thursday");
       break;
-      case 5:
+    case 5:
       print("Today is Friday");
       break;
-      case 6:
+    case 6:
       print("Today is Saturday");
       break;
-      case 7:
+    case 7:
       print("Today is Sunday");
       break;
 
-    default: print("INVALID day");
+    default:
+      print("INVALID day");
   }
 
+  //EJEMPLO 2 combinar varios casos switch:
 
+  final dayDay = 4;
 
-  
+  switch (dayDay) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      print("Working day");
+      break;
+
+    case 6:
+    case 7:
+      print("Free day");
+      break;
+
+    default:
+      print("Day off");
+  }
 }
