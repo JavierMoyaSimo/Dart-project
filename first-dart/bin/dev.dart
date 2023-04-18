150c2b6cbf50c195d70991f7b3345249a5e11a47
Javi <javiermoyasimo@gmail.com>
2023-04-18 14:34:00 +0200

    feat: work with null safety

--- first-dart/bin/dev.dart
+++ first-dart/bin/dev.dart
@@ -5,13 +5,8 @@ void main(List<String> arguments) {
   counter = 2.3;
   print(sum(4, counter));
 
-
-
-
   // CLASE PADRE OBJECT
   Object obj;
-
-   
 }
 
 ///return the sum of a with b
@@ -29,8 +24,6 @@ double sum(int a, double b) {
   return a + b;
 }
 
-
-
 //--------------------------------------------------------------------------------
 
 // DATOS INMUTABLES
@@ -40,16 +33,37 @@ final // Su valor Puede ser asignado en tiempo de ejecución
 const // Su valor debe ser asignado antes de compilar
 */
 
-
 //Definir variables inmutables:
 final String userName1 = "Javi";
 final userName2 = "Pepe";
 
-const String userName3= "Juan";
+const String userName3 = "Juan";
 const userName4 = "Paco";
 
+///////////////////////////////////// NULL SAFETY:////////////////////////////////////
+
+/* En pubspec.yaml:
 
 
 
+EN: 
 
+environment:
+  sdk: '>=2.19.6 <3.0.0'
 
+  Aqui tenemos la version de nuestro kit de desarrollo de software de Dart.
+
+  Para que actualice las nuevas dependencias instaladas en dicho archivo, etc:
+  en consola:
+
+  dart pub get   (Lee nuestro archivo pubspec.yaml).
+
+ */
+
+void main2() {
+  String? name = "Javier"; // con ? , la variable name acepta valor: null. 
+  print(name.length); // 6
+  name = null;
+  print(name);// null
+  print(name?.length);// con name?.length NO CRASHEA la web ( podria retornar nulo)
+}
