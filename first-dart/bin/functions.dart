93979c7ef2a2352f9f04e88033caf40460bd00d9
Javi <javiermoyasimo@gmail.com>
2023-04-19 00:58:45 +0200

    feat: calculateAge function added

--- first-dart/bin/functions.dart
+++ first-dart/bin/functions.dart
@@ -7,10 +7,20 @@ void main() {
 
 //Dias de diferencia entre la fecha actual y la fecha de nacimiento
   final daysOfDifferenceDateAndBirthday =
-      birthdayDate.difference(currentDate).inDays;
+      currentDate.difference(birthdayDate).inDays;
 
 //Dias de diferencia / 1 año    ---- devuelve: EDAD
   print(daysOfDifferenceDateAndBirthday / 365);
+
+//UTILIZANDO LA FUNCION!!
+  print(calculateAge(birthdayDate, 1));
+
+  final birthdayDate2 = DateTime(1999, 1, 23);
+  print(calculateAge(birthdayDate2, 1));
+
+  //Para REDONDEAR el numero double que retorna la funcion, usaremos el SEGUNDO PARAMETRO de la funcion, para indicarle cuantos decimales queremos retornar.
+  //El metodo toStringAsFixed(numeroDECIMALES) retorna un STRING del numero con dichos decimales(numeroDECIMALES).
+  //En la FUNCION, return double.parse(dichometodo) para pasarlo a un numero DOUBLE.
 }
 
 //FUNCION para CALCULAR LA EDAD
@@ -20,8 +30,11 @@ void main() {
 //...
 // return ....;}
 
-double calculateAge(DateTime birthdayDate) {
+double calculateAge(DateTime birthdayDate, int precision) {
   final current = DateTime.now();
-  final days = birthdayDate.difference(current).inDays;
-  return days / 365;
+  final days = current.difference(birthdayDate).inDays;
+  final age = days / 365;
+  return double.parse(
+    age.toStringAsFixed(precision),
+  );
 }
