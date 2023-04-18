fcc1801ef248da09085e70844e33675e9441dee4
Javi <javiermoyasimo@gmail.com>
2023-04-19 01:06:34 +0200

    feat: third parameter added

--- first-dart/bin/functions.dart
+++ first-dart/bin/functions.dart
@@ -13,28 +13,37 @@ void main() {
   print(daysOfDifferenceDateAndBirthday / 365);
 
 //UTILIZANDO LA FUNCION!!
-  print(calculateAge(birthdayDate, 1));
+  print(calculateAge(birthdayDate, 1,true));
 
   final birthdayDate2 = DateTime(1999, 1, 23);
-  print(calculateAge(birthdayDate2, 1));
+  print(calculateAge(birthdayDate2, 1,true));
 
   //Para REDONDEAR el numero double que retorna la funcion, usaremos el SEGUNDO PARAMETRO de la funcion, para indicarle cuantos decimales queremos retornar.
   //El metodo toStringAsFixed(numeroDECIMALES) retorna un STRING del numero con dichos decimales(numeroDECIMALES).
   //En la FUNCION, return double.parse(dichometodo) para pasarlo a un numero DOUBLE.
 }
 
-//FUNCION para CALCULAR LA EDAD
 
+//FUNCION para CALCULAR LA EDAD
 //tipoDatoqueDevuelvelaFuncion nombreFuncion (tipoDatoPARAMETRO PARAMETRO) {
 //......
 //...
 // return ....;}
 
-double calculateAge(DateTime birthdayDate, int precision) {
+//PARAMETROS(FechaCumpleaños, DecimalesAIncluir, BuleanoParaDevolverUnaEdadUOtra)
+
+double calculateAge(DateTime birthdayDate, int precision, bool round) {
+
+  
   final current = DateTime.now();
   final days = current.difference(birthdayDate).inDays;
   final age = days / 365;
+  if(round) {
+    //utilizamos .roundToDouble porque la funcion "calculateAge" tiene que retornar un DOUBLE
+   return age.roundToDouble();
+  }
   return double.parse(
     age.toStringAsFixed(precision),
   );
+  
 }
