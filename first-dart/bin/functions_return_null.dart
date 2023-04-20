1cd86bea2b922f3f89befaac3e5eb9530d4167b3
Javi <javiermoyasimo@gmail.com>
2023-04-20 15:35:22 +0200

    feat: working with nulls in returns functions

---
+++ first-dart/bin/functions_return_null.dart
@@ -0,0 +1,48 @@
+//FUNCION QUE RETORNA UN DATO NULO:
+
+void main() {
+  print(calculateAge(""));
+}
+
+//para que una FUNCION PUEDA RETORNAR NULL, basta con poner ? despues del tipo de dato a retornar
+// observar que el TIPO DE DATO que RETORNA la FUNCION es double?-------- o double o null!!
+double? calculateAge(String birthdayAsString) {
+  final current = DateTime.now();
+
+  //PRIMER PASO: Pasamos el parametro birthdayAsString a un dato de tipo DATETIME:
+  //tryParse () retorna NULL si no puede convertir el STRING a DATETIME.
+  final birthday = DateTime.tryParse(birthdayAsString);
+
+//si no es null:
+  if (birthday != null) {
+    final days = current.difference(birthday).inDays;
+    final age = days / 365;
+
+    return double.parse(
+      age.toStringAsFixed(1),
+    );
+  }
+
+  //Si es nulo:
+  return null;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+/// Si queremos que la funcion NO RETORNE ningun tipo de dato, sino que simplemente realice alguna accion:
+/// llamamos a la funcion:   void  nombreFunction () {}
+///
+
+void functionOne(String birthdayAsString) {
+  final current = DateTime.now();
+
+  final birthday = DateTime.tryParse(birthdayAsString);
+
+  if (birthday != null) {
+    final days = current.difference(birthday).inDays;
+    final age = days / 365;
+
+    double.parse(
+      age.toStringAsFixed(1),
+    );
+  }
+}
